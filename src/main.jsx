0f82305e131df248f4f1d0cd3e847dc0171c366b
Developer <himanshu.masterbazar@gmail.com>
2025-10-31 20:56:00 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,24 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter, useLocation } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 function ScrollToTop() {
   const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
   }, [pathname]);
   return null;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <ScrollToTop />
-
       <Suspense fallback={<div className="p-6">Loading…</div>}>
         <App />
       </Suspense>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
