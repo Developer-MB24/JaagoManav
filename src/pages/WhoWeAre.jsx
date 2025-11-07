fce1d19668c7ecdc3e79a179086f5bf8879de73f
Developer <himanshu.masterbazar@gmail.com>
2025-11-07 22:23:06 +0530

    new

--- src/pages/WhoWeAre.jsx
+++ src/pages/WhoWeAre.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HeroWhoWeAre from "../components/whoweare/HeroWhoWeAre";
-import WhoWeAreSection from "../components/whoweare/WhoWeAreSection";
+
 import CompanyStatsSection from "../components/whoweare/CompanyStatsSection";
 import JourneySection from "../components/whoweare/JourneySection";
 import VolunteerCard from "../components/whoweare/VolunteerCard";
@@ -8,6 +8,7 @@ import EconestUpcomingEvents from "../components/whoweare/EconestUpcomingEvents.
 
 import ContactBar from "../components/whoweare/ContactBar";
 import WhoWeAreTestimonial from "../components/whoweare/WhoWeAreTestimonial";
+import WhoWeAreSection from "../components/WhoWeAre/WhoWeAreSection.jsx";
 
 export default function Whoweare() {
   return (
